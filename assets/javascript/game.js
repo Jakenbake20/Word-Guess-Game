9209a5e53c3bf872b655ef3ae5a765916db7bbdc
Jakenbake <jacob_wheat@yahoo.com>
2020-03-10 11:12:15 -0600

    added images for both background texture and text background. Made basic functions of word guessing game. Modified both headers and added a border to h2. Still need boxes, places for functions to function and music.

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+var guessingGame = alert("Welcome to Coding Karaoke! Press ok to get started.");{
+    console.log("alert here");
+}
+
+ //initial array and variables. Global scopes.
+    var game=["Beat it", "Welcome to the Jungle", ""]
+    var choice= Math.floor(Math.random()*4);
+    var answer = game[choice];
+    var myLength = answer.length;
+    var display=[myLength];
+    var win=myLength;
+    var letters = answer.split('')
+    var attemptsLeft= 10;
+    var output="";
+    var userLetter="";
+
+var setup = function(){
+
+    for (var i=0; i<answer.length; i++){
+        display[i] = "_ ";
+        output = output+display[i];
+    }
+    document.getElementById('game').innerHTML=output;
+    output ="";
+}
+
+window.onload=function(){
+    setup()
+}
+
+
+
+
+
+
+
+
